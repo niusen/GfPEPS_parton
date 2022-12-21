94c9bcd99fa45a45694b6932a02c1baf0c6392e3
niusen <1501110187@pku.edu.cn>
2022-12-21 13:36:36 +0100

    Update build_tensor.jl

--- GfPEPS_ctmrg/build_tensor.jl
+++ GfPEPS_ctmrg/build_tensor.jl
@@ -25,10 +25,18 @@ E0=load(init_state)["E0"];
 Vf=ℂ[FermionNumber](0=>1, 1=>1);
 U2=unitary(fuse(Vf ⊗ Vf), Vf ⊗ Vf)
 
-
 A_set=create_vaccum(2);
-mpo_set=build_mpo_set(2,[0.6,0.4]);
-
+mpo_set=build_mpo_set([0.6,0.4]);
 A_set_new=mpo_mps(mpo_set,A_set)
-
 @tensor A_total[:]:=A_set_new[1][-1,1,-2]*A_set_new[2][1,-4,-3]
+
+
+A_set=create_vaccum(3);
+mpo_set=build_mpo_set([0.6,0.4,0.2]);
+A_set_new=mpo_mps(mpo_set,A_set)
+@tensor A_total[:]:=A_set_new[1][-1,1,-2]*A_set_new[2][1,2,-3]*A_set_new[3][2,-5,-4]
+
+A_set=create_vaccum(4);
+mpo_set=build_mpo_set([0.6,0.4,0.2,0.1]);
+A_set_new=mpo_mps(mpo_set,A_set)
+@tensor A_total[:]:=A_set_new[1][-1,1,-2]*A_set_new[2][1,2,-3]*A_set_new[3][2,3,-4]*A_set_new[4][3,-6,-5]
